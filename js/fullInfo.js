77750d9826e6f607d4efd45813cc3c08b589be68
diyor <dierismailov9@gmail.com>
2021-12-18 00:47:18 +0500

    add pop-up

--- js/fullInfo.js
+++ js/fullInfo.js
@@ -9,12 +9,29 @@ const   sideBarBtn = document.querySelector('.side-bar__btn'),
         dropdownBtn = document.querySelector('.side-bar__dropdown'), 
         tableTabs = document.querySelectorAll('.return__item'),
         tables = document.querySelectorAll('.table'),
+        selectBtns = document.querySelectorAll('.select__btn'),
+        selectParent = '.select',
+        selectItems = document.querySelectorAll('.select__item'),
         detailsContent = document.querySelector('.details'),
         screenMore = document.querySelector('.details__user'),
         seeMore = document.querySelector('.details__history-link'),
         historyCommit = document.querySelector('.details__history-events'),   
         warningMsg = document.querySelector('.warning'),
-        warningClose = document.querySelector('.warning__close');
+        warningClose = document.querySelector('.warning__close'),
+        textareaParent = ".pop-up",
+        textareasPopUp = document.querySelectorAll('.pop-up__textarea'),
+        symbolCurent = '.pop-up__symbols-curent',
+        symbolMax = '.pop-up__symbols-max',
+        radioChoose = document.querySelectorAll('.choose__label'),
+        detailBtns = document.querySelectorAll('.details__btn'),
+        popUpBg = document.querySelector('.pop-up__wrapper'),
+        popUpBlock = document.querySelectorAll('.pop-up__block'),
+        popUpClose = document.querySelectorAll('.pop-up__close'),
+        popUpCheck = document.querySelectorAll('.pop-up__input'), 
+        popUpEdit = document.querySelectorAll('.pop-up__edit-icon'),
+        popUpEmail = '.pop-up__label-hidden',
+        popUpCheckParent = '.pop-up__changing-block',
+        body = document.querySelector('body');
     
 sideBarBtn.addEventListener('click', () => {
     sideBar.classList.toggle('active'); 
@@ -103,4 +120,102 @@ seeMore.addEventListener('click', () => {
 warningClose.addEventListener('click', () => {
     warningMsg.classList.add('hide');
     setTimeout(() => {warningMsg.classList.add('close')},400)
+})
+
+selectBtns.forEach(btn => { 
+    btn.addEventListener('click', () => {  
+        document.querySelectorAll(selectParent).forEach(parent => {
+            if(btn.closest(selectParent) != parent){
+                parent.classList.remove('show-select')
+            }
+        })
+        btn.closest(selectParent).classList.toggle('show-select') 
+    });
+}) 
+
+selectItems.forEach(item => {
+     item.addEventListener('click', () => {
+        selectItems.forEach(el => {
+            el.classList.remove('item-selected')
+        })
+        item.classList.add('item-selected'); 
+        item.closest(selectParent).children[0].textContent = item.getAttribute('data-value');
+        item.closest(selectParent).classList.remove('show-select')
+     })
+})  
+
+textareasPopUp.forEach(writeTable => {
+    let parent =  writeTable.closest(textareaParent);
+    parent.querySelector(symbolMax).textContent = writeTable.getAttribute('maxlength');
+    writeTable.value = 'Gets filled based selection on top. Can be edited if Others is selected in top drop down.'
+    writeTable.addEventListener('input', () => { 
+       parent.querySelector(symbolCurent).textContent = writeTable.value.length;  
+    })
+})
+
+radioChoose.forEach(elem => {
+    elem.addEventListener('click', () => { 
+        radioChoose.forEach(k => {
+            k.classList.remove('active')
+        })
+        if (elem.querySelector('input').checked) {
+            elem.classList.add('active')
+        }
+    })
+})
+
+detailBtns.forEach(button => {
+    button.addEventListener('click', () => { 
+        button.classList.add('active');
+        popUpBg.classList.add('show');
+        let btnId = button.getAttribute('data-id'); 
+        document.querySelector('#'+ btnId).classList.add('active');
+        body.style.overflow = 'hidden'
+    })
+})
+
+popUpClose.forEach(closeBtn => {
+    closeBtn.addEventListener('click', () => {
+        popUpBlock.forEach(block => {
+            block.classList.remove('active')
+        })
+        detailBtns.forEach(btns => {
+            btns.classList.remove('active')
+        })
+        popUpBg.classList.remove('show');
+        body.style.overflow = 'visible'
+    })
+})
+
+popUpBg.addEventListener('click', (e) => {
+    if(e.target === popUpBg ) { 
+        popUpBlock.forEach(block => {
+            block.classList.remove('active')
+        })
+        detailBtns.forEach(btns => {
+            btns.classList.remove('active')
+        })
+        popUpBg.classList.remove('show');
+        body.style.overflow = 'visible'
+    }
+})
+
+popUpCheck.forEach(inp => {
+    inp.addEventListener('change', () => {
+        let parent = inp.closest(popUpCheckParent);
+        if(inp.checked) { 
+            parent.querySelector(popUpEmail).classList.add('show')
+        }else  parent.querySelector(popUpEmail).classList.remove('show')
+    })
+})
+
+popUpEdit.forEach(edit => {
+    edit.addEventListener('click', () => { 
+        let el = edit.previousElementSibling;
+        el.disabled = false;  
+        el.setAttribute('data-value', el.value); 
+        el.addEventListener('change', () => {
+            el.disabled = true;
+        })
+    })
 })
